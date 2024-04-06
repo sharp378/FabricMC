f516893280fb881fd859510c020d0050fb745358
sharp378 <107257160+sharp378@users.noreply.github.com>
2024-04-06 17:27:29 -0400

    fix some issues with the scripts (#4)

--- get-fabric-latest.sh
+++ get-fabric-latest.sh
@@ -3,8 +3,10 @@
 minecraft_ver="$1"
 
 # get latest stable fabric loader & installer
-loader_ver=$(curl 'https://meta.fabricmc.net/v2/versions/loader' | jq 'map(select(.stable == true)).[0].version')
-installer_ver=$(curl 'https://meta.fabricmc.net/v2/versions/installer' | jq 'map(select(.stable == true)).[0].version')
+loader_ver=$(curl 'https://meta.fabricmc.net/v2/versions/loader' | jq 'map(select(.stable == true))' | jq '.[0].version')
+installer_ver=$(curl 'https://meta.fabricmc.net/v2/versions/installer' | jq 'map(select(.stable == true))' | jq '.[0].version')
 
 # build server download url
-url="https://meta.fabricmc.net/v2/versions/loader/${minecraft_ver}/${loader_ver}/${installer_ver}/server/jar 
+url="https://meta.fabricmc.net/v2/versions/loader/${minecraft_ver}/${loader_ver//\"/}/${installer_ver//\"/}/server/jar"
+echo "$url"
+
